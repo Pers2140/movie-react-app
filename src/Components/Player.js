8107966386ca75fe686fd97d124d7c84796e0b0f
Pers2140 <Pers2140@fredonia.edu>
2022-06-15 15:29:43 +0000

    fixed fullscreen bug

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 export default function Player({style, moviesrc,setMovie}) {
     
-    
-   
     const closePlayer = () =>{
         setMovie('default')
+        if (document.cancelFullScreen) {
+          document.cancelFullScreen();
+       } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+       } else if (document.webkitCancelFullScreen) {
+         document.webkitCancelFullScreen();
+       }
     }
     const changeFullScreen = () => {
 
@@ -34,7 +39,7 @@ export default function Player({style, moviesrc,setMovie}) {
         <div className="movie-player" style={style}>
         <div className="cover-bar">
             <div style = {{"fontSize":"32px",position:"absolute",right:0}} onClick={changeFullScreen} className="toggle-fulscreen">↗️</div>
-            <div style = {{"fontSize":"32px",position:"absolute",left:0}} onClick={()=>{closePlayer();changeFullScreen()}} className="return-button">⬅️</div>
+            <div style = {{"fontSize":"32px",position:"absolute",left:0}} onClick={closePlayer} className="return-button">⬅️</div>
         </div>
         <iframe title={"this is unique"}  style={{width:"100%",height:"100%"}}src={moviesrc}></iframe> 
         </div>
